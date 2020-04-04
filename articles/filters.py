43da59b861c0287552ce7e401a07f37da820b244
Amjad Masad <amjad.masad@gmail.com>
2020-04-04 19:12:36 +0000

    added col-timezone

--- articles/filters.py
+++
@@ -1,8 +0,0 @@
-import django_filters
-from .models import Articles
-
-class ArticleFilter(django_filters.FilterSet):
-  
-  class Meta:
-    model= Articles
-    fields= ('title', 'snippets','date', 'author')
